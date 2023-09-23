396685c95dc8591edde9cc0a4645445c4ef303da
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-09-23 04:08:26 -0400

    created new logic for pet list

--- src/components/PetsList.jsx
+++ src/components/PetsList.jsx
@@ -1,69 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; 
-import PetProfile from './PetProfile';
-import Notification from './Notifications';
+import React, { useEffect, useState } from 'react';
+import { getPetById} from './api';
 
-export default function PetList({ pets }) {
-  const [selectedProfiles, setSelectedProfiles] = useState([]);
-  const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem('favorites')) || []);
-  const [showNotification, setShowNotification] = useState(false);
-  const [notificationMessage, setNotificationMessage] = useState('');
-
-  const handleProfileClick = (profile) => {
-    const isProfileSelected = selectedProfiles.some(
-      (selected) => selected.id === profile.id
-    );
-
-    const isProfileInPetList = pets.some((pet) => pet.id === profile.id);
-    const isProfileInFavorites = favorites.some((fav) => fav.id === profile.id);
-
-    if (!isProfileSelected && !isProfileInFavorites && isProfileInPetList) {
-      setSelectedProfiles((prevSelected) => [...prevSelected, profile]);
-    }
-  };
-
-  const saveFavorites = () => {
-    localStorage.setItem('favorites', JSON.stringify([...favorites, ...selectedProfiles]));
-    setFavorites([...favorites, ...selectedProfiles]);
-    setSelectedProfiles([]);
-
-    if (selectedProfiles.length > 0) {
-      const petNames = selectedProfiles.map((profile) => profile.name).join(', ');
-      const petID = selectedProfiles.map((profile) => profile.id).join(', ');
-      const newNotificationMessage = `${petNames} have been added to favorites.`;
-      setNotificationMessage(newNotificationMessage);
-      setShowNotification(true);
-    }
-  };
+function PetList() {
+  const [pets, setPets] = useState([]);
 
   useEffect(() => {
-    if (showNotification) {
-      const timeoutId = setTimeout(() => {
-        setShowNotification(false);
-      }, 3000);
-
-      return () => clearTimeout(timeoutId);
-    }
-  }, [showNotification]);
+    // Fetch pet data from the API
+    getPetById()
+      .then((data) => {
+        setPets(data.animals);
+      })
+      .catch((error) => {
+        console.error('Error fetching pet data:', error);
+      });
+  }, []);
 
   return (
-    <div className="pet-list">
-      <button onClick={saveFavorites}>Save Selected as Favorites</button>
-      {pets.map((pet) => (
-        <div key={pet.id}>
-            <PetProfile
-              pet={pet}
-              isSelected={selectedProfiles.some((selected) => selected.id === pet.id)}
-              onPetSelect={() => handleProfileClick(pet)}
-            />
-                 </div>
-      ))}
-      {showNotification && (
-        <Notification
-          message={notificationMessage}
-          onClose={() => setShowNotification(false)}
-        />
-      )}
+    <div>
+      <h2>Pet List</h2>
+      <ul>
+        {pets.map((pet) => (
+          <li key={pet.id}>
+            <div>
+              <img src={pet.photos[0]?.medium} alt={pet.name} />
+              <p>{pet.name}</p>
+              {/* Add other pet details and actions here */}
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export default PetList;
+
